e101b93e66556807dfd46c541c821b750cf49f79
Max Lund <maxlun@gmail.com>
2018-01-19 15:42:05 +0100

    revise debug.. no time

--- Lab0/find_bug.c
+++ Lab0/find_bug.c
@@ -7,21 +7,24 @@ int main(int argc, char ** argv)
     char *buf[9]; // char* array of size 9
     char **bufi, **bufend; // two pointers to char*
     bufi = buf; // bufi points to start of buf, i.e. points to the first char*
-    bufend = &buf[9]; // bufend points to the last char* in buf
+    bufend = &buf[9]; // bufend points to char at buf[9] ? we only hold 9 element, last index is 8?
 
     while (bufi != bufend){ // fill buf with chars from str in reverse order
 	*bufi = stri;
 	bufi++;
 	stri--;
     }
-
+    
+    int i = 0;
     while (bufi != buf){ 
-	// (*bufi) -= 32;  this line is the bug, we try to decrement the char pointer by 32
+// we dereference the char* and decrement char value by 3, but if first pointed to is beyond char array, segfault?
+//	*(*bufi) -= 32;
 	bufi--; // we decrement bufi to be the beginning of buf array again
+        *(*bufi) -= 32; // decrement by 32 after decrementing bufi instead
     }
-
     while (bufi != bufend){ // we print the chars in buf
-	printf("%c", **bufi);
-	bufi++;
+    	printf("%c", **bufi);
+    	bufi++;
     }
+    printf("\n");
 }
