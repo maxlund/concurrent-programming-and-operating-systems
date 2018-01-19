793557774873341ece73a3d7080fa3aca4094ec2
Max Lund <maxlun@gmail.com>
2018-01-19 11:53:01 +0100

    functional linked_list

--- Lab0/linked_list.c
+++ Lab0/linked_list.c
@@ -6,11 +6,14 @@ struct list_item
 {
     int value;
     struct list_item *next;
+    
 };
 
 void append(struct list_item *first, int x)
 {
-    while (first->next != NULL) first = first->next;
+    while (first->next != NULL) 
+	first = first->next;
+
     first->next = (struct list_item*)malloc(sizeof(struct list_item));
     first->next->value = x;
 }
@@ -20,6 +23,7 @@ void prepend(struct list_item *first, int x)
     struct list_item *n = (struct list_item*)malloc(sizeof(struct list_item));
     n->next = first->next;
     first->next = n;
+    n->value = x;
 }
 
 void print(struct list_item *first)
@@ -27,7 +31,39 @@ void print(struct list_item *first)
     while (first->next != NULL)
     {
 	first = first->next;
-	printf("%d", first->value);
+	printf("%d ", first->value);
+    }
+    printf("\n");
+}
+
+void input_sorted(struct list_item *first, int x)
+{
+    if (first->next == NULL) 
+    {
+	prepend(first, x);
+    }
+    else 
+    {
+	if (first->next->value > x)
+	{
+	    struct list_item *n = (struct list_item*)malloc(sizeof(struct list_item));
+	    n->next = first->next;
+	    n->value = x;
+	    first->next = n;
+	}
+	else
+	{
+	    input_sorted(first->next, x);
+	}
+    }
+}
+void clear(struct list_item *first)
+{
+    while (first->next != NULL)
+    {
+	struct list_item* to_free = first->next;
+	first = first->next;
+	free(to_free);
     }
 }
 
@@ -37,5 +73,23 @@ int main( int argc, char ** argv)
     root.value = -1; /* This value is always ignored */
     root.next = NULL;
 
-    // destructors / free ??
+    // test append
+    append(&root, 10);
+    append(&root, 20);
+    append(&root, 30);
+    append(&root, 2);
+    print(&root); // should print 10, 20, 30, 2
+
+    // test prepend
+    prepend(&root, 8);
+    prepend(&root, 5);
+    print(&root); // should print 5, 8, 10, 20, 30, 2
+
+    // test input_sorted
+    input_sorted(&root, 25);
+    input_sorted(&root, 9);
+    input_sorted(&root, 1);
+    input_sorted(&root, 100);
+    print(&root); // should print 1, 5, 8, 9, 10, 20, 25, 20, 2, 100
 }
+
